6d1056e8e3af3265f0a974c7dbcaec590c7d8ee3
Kaden Wong <kdw2x@icloud.com>
2025-05-05 16:25:04 -0700

    Fix: Remove Data = Get it urself idk = Clean code

--- createMap.py
+++ createMap.py
@@ -1,12 +1,13 @@
 import folium, json, os
 
 map = folium.Map(
-            prefer_canvas = True,
-            tiles = "Cartodb Voyager",
-            zoom_start = 12,
-            min_zoom = 11,
-            location = [22.345064, 114.190009]
-                )
+    prefer_canvas = True,
+    tiles = "Cartodb Voyager",
+    zoom_start = 12,
+    min_zoom = 11,
+    location = [22.345064, 114.190009]
+    )
+folium.LayerControl().add_to(map)
 
 route1 = folium.FeatureGroup(name = "1", show = False).add_to(map)
 route2 = folium.FeatureGroup(name = "2", show = False).add_to(map)
@@ -18,8 +19,6 @@ route7 = folium.FeatureGroup(name = "7", show = False).add_to(map)
 route8 = folium.FeatureGroup(name = "8", show = False).add_to(map)
 route9 = folium.FeatureGroup(name = "9", show = False).add_to(map)
 routeX = folium.FeatureGroup(name = "Other", show = False).add_to(map)
-
-folium.LayerControl().add_to(map)
 
 file = open("data/output.json")
 fileData = file.read()
@@ -78,29 +77,26 @@ def setData(name, type):
         elif type == "layer":
             return layers[int(strippedName[0]) - 1]
 
-for j in range(len(fileObject["features"]) - 1):
-    lat1 = fileObject["features"][j]["geometry"]["coordinates"][1]
-    lon1 = fileObject["features"][j]["geometry"]["coordinates"][0]
-    lat2 = fileObject["features"][j + 1]["geometry"]["coordinates"][1]
-    lon2 = fileObject["features"][j + 1]["geometry"]["coordinates"][0]
+for i in range(len(fileObject["features"]) - 1):
+    lat1 = fileObject["features"][i]["geometry"]["coordinates"][1]
+    lon1 = fileObject["features"][i]["geometry"]["coordinates"][0]
+    lat2 = fileObject["features"][i + 1]["geometry"]["coordinates"][1]
+    lon2 = fileObject["features"][i + 1]["geometry"]["coordinates"][0]
 
-    if fileObject["features"][j]["properties"]["routeNameE"] == fileObject["features"][j + 1]["properties"]["routeNameE"]:
+    if fileObject["features"][i]["properties"]["routeNameE"] == fileObject["features"][i + 1]["properties"]["routeNameE"]:
         if abs(lat2 - lat1) <= 0.05 and abs(lon2 - lon1) <= 0.05:
-            if fileObject["features"][j + 1]["properties"]["stopSeq"] - fileObject["features"][j]["properties"]["stopSeq"] == 1:
-
-
+            if fileObject["features"][i + 1]["properties"]["stopSeq"] - fileObject["features"][i]["properties"]["stopSeq"] == 1:
                 plotLine(
                     lat1, lon1,
                     lat2, lon2,
-                    setData(fileObject["features"][j]["properties"]["routeNameE"], "color"),
-                    fileObject["features"][j]["properties"]["routeNameE"],
-                    setData(fileObject["features"][j]["properties"]["routeNameE"], "layer")
+                    setData(fileObject["features"][i]["properties"]["routeNameE"], "color"),
+                    fileObject["features"][i]["properties"]["routeNameE"],
+                    setData(fileObject["features"][i]["properties"]["routeNameE"], "layer")
                 )
 
 subfolder = "output"
 filename = "map.html"
 full_path = os.path.join(subfolder, filename)
-
 os.makedirs(subfolder, exist_ok = True)
 
 map.save(full_path)
