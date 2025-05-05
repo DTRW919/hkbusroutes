560fe8470aff136b2a8806e124740d631034bc35
Kaden Wong <kdw2x@icloud.com>
2025-05-05 16:19:19 -0700

    Feat: Layers for Routes + Layers for every number = Combine some functions + Plot points for fun

--- createMap.py
+++ createMap.py
@@ -8,28 +8,39 @@ map = folium.Map(
             location = [22.345064, 114.190009]
                 )
 
+route1 = folium.FeatureGroup(name = "1", show = False).add_to(map)
+route2 = folium.FeatureGroup(name = "2", show = False).add_to(map)
+route3 = folium.FeatureGroup(name = "3", show = False).add_to(map)
+route4 = folium.FeatureGroup(name = "4", show = False).add_to(map)
+route5 = folium.FeatureGroup(name = "5", show = False).add_to(map)
+route6 = folium.FeatureGroup(name = "6", show = False).add_to(map)
+route7 = folium.FeatureGroup(name = "7", show = False).add_to(map)
+route8 = folium.FeatureGroup(name = "8", show = False).add_to(map)
+route9 = folium.FeatureGroup(name = "9", show = False).add_to(map)
+routeX = folium.FeatureGroup(name = "Other", show = False).add_to(map)
+
+folium.LayerControl().add_to(map)
+
 file = open("data/output.json")
 fileData = file.read()
 fileObject = json.loads(fileData)
 
-def plotDot(lat, lon, color, name):
+def plotLine(lat1, lon1, lat2, lon2, color, name, layer):
     folium.CircleMarker(
-                    location = [lat, lon],
-                    radius = 2,
-                    weight = 1,
-                    color = color,
-                    tooltip = name
-                       ).add_to(map)
+        location = [lat1, lon1],
+        radius = 2,
+        weight = 1,
+        color = color
+    ).add_to(layer)
 
-def plotLine(lat1, lon1, lat2, lon2, color, name):
     folium.PolyLine(
-                    locations = [[lat1, lon1], [lat2, lon2]],
-                    weight = 2,
-                    color = color,
-                    tooltip = name
-                       ).add_to(map)
+        locations = [[lat1, lon1], [lat2, lon2]],
+        weight = 2,
+        color = color,
+        tooltip = name
+    ).add_to(layer)
 
-def findColor(name):
+def setData(name, type):
     colors = [
         "red",
         "orange",
@@ -41,16 +52,31 @@ def findColor(name):
         "darkred",
         "lightgreen",
         "darkblue"
-             ]
+        ]
+    layers = [
+        route1,
+        route2,
+        route3,
+        route4,
+        route5,
+        route6,
+        route7,
+        route8,
+        route9,
+    ]
 
     strippedName = ''.join(char for char in name if char.isdigit())
 
     if not strippedName:
-        return "black"
-
-    return colors[int(strippedName[0]) - 1]
-
-
+        if type == "color":
+            return "black"
+        elif type == "layer":
+            return routeX
+    else:
+        if type == "color":
+            return colors[int(strippedName[0]) - 1]
+        elif type == "layer":
+            return layers[int(strippedName[0]) - 1]
 
 for j in range(len(fileObject["features"]) - 1):
     lat1 = fileObject["features"][j]["geometry"]["coordinates"][1]
@@ -63,7 +89,13 @@ for j in range(len(fileObject["features"]) - 1):
             if fileObject["features"][j + 1]["properties"]["stopSeq"] - fileObject["features"][j]["properties"]["stopSeq"] == 1:
 
 
-                plotLine(lat1, lon1, lat2, lon2, findColor(fileObject["features"][j]["properties"]["routeNameE"]), fileObject["features"][j]["properties"]["routeNameE"])
+                plotLine(
+                    lat1, lon1,
+                    lat2, lon2,
+                    setData(fileObject["features"][j]["properties"]["routeNameE"], "color"),
+                    fileObject["features"][j]["properties"]["routeNameE"],
+                    setData(fileObject["features"][j]["properties"]["routeNameE"], "layer")
+                )
 
 subfolder = "output"
 filename = "map.html"
